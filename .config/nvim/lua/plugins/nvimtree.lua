3829c9218578190b3ed0dcc1d9da9cc3fe57164b
Shootertrex <shootertrex@gmail.com>
2025-02-14 01:23:16 -0500

    fixed 'h' and 'l' not opening and collapsing folders

--- .config/nvim/lua/plugins/nvimtree.lua
+++ .config/nvim/lua/plugins/nvimtree.lua
@@ -9,8 +9,9 @@ return {
   dependencies = {
     "nvim-tree/nvim-web-devicons",
   },
-  config = function()
-    require("nvim-tree").setup {
+  config = function ()
+    local api = require("nvim-tree.api")
+    require"nvim-tree".setup {
       actions = {
         open_file = {
           quit_on_open = true,
@@ -19,6 +20,35 @@ return {
           },
         },
       },
+      on_attach = function (bufnr)
+        local opts = { buffer = bufnr }
+        api.config.mappings.default_on_attach(bufnr)
+        local lefty = function ()
+          local node_at_cursor = api.tree.get_node_under_cursor()
+          -- node_at_cursor.nodes to checks if has nodes (is a folder) and .open to check it's open
+          if node_at_cursor.nodes and node_at_cursor.open then
+            -- .open.edit() toggles the node (folder) from open to closed
+            api.node.open.edit()
+          else
+            -- if instead node_at_cursor is a file or a closed folder, jump to the parent node (folder)
+            api.node.navigate.parent()
+          end
+        end
+        local righty = function ()
+          local node_at_cursor = api.tree.get_node_under_cursor()
+          -- node_at_cursor.nodes to checks if has nodes (is a folder) and not .open to check it's closed
+          if node_at_cursor.nodes and not node_at_cursor.open then
+            -- .open.edit() toggles the node (folder) from closed to open
+            api.node.open.edit()
+            -- no else here, if it's an open folder or a file do nothing
+          end
+        end
+        vim.keymap.set("n", "h", lefty , opts )
+        vim.keymap.set("n", "<Left>", lefty , opts )
+        vim.keymap.set("n", "<Right>", righty , opts )
+        vim.keymap.set("n", "l", righty , opts )
+      end,
     }
-  end,
+  end
 }
+
